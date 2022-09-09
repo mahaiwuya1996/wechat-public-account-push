38281477d2ac15cdd776663aeb22ff1e62e55c21
fallinlove <42397863+mahaiwuya1996@users.noreply.github.com>
2022-09-09 16:46:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx11d71e3bd9fc8026",
+  APP_ID: "wxa862f5498466dbd4",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
- APP_SECRET: "638b2f0094b2ec0bfc36ecde47022ba6",
+ APP_SECRET: "bebd978759965a938325a179312a5358",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
