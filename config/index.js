2e3a809a851987f583e3a2a2ae586d554100b7f9
fallinlove <42397863+mahaiwuya1996@users.noreply.github.com>
2022-09-09 17:36:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "iiMA0D2lAAr0IDFcr2PaHyCdOYpw0QeADcweGKazoq4",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o76lI645_jcUQ29TryGf6aACbZlg",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "dg2I-lmt2oME6cwDSu4nG6ypPRy1poewMqdcXB6C-ug",
+      useTemplateId: "iiMA0D2lAAr0IDFcr2PaHyCdOYpw0QeADcweGKazoq4",
       // 所在省份
       province: "广东",
       // 所在城市
@@ -50,7 +50,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "dg2I-lmt2oME6cwDSu4nG6ypPRy1poewMqdcXB6C-ug",
+  CALLBACK_TEMPLATE_ID: "iiMA0D2lAAr0IDFcr2PaHyCdOYpw0QeADcweGKazoq4",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
