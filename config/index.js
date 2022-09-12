bab86b9240a190f213cee09790b37a2dbc8a2180
fallinlove <42397863+mahaiwuya1996@users.noreply.github.com>
2022-09-12 12:34:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -42,6 +42,20 @@ export const config = {
       city: "深圳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://s.weibo.com/"
+    },
+    {
+      // 想要发送的人的名字
+      name: "dear 熊丽晴",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "o76lI645_jcUQ29TryGf6aACbZlg",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "_T3ytr0zUynKcgT-G55_BVbhckWkrjmEzWvV8MySMis",
+      // 所在省份
+      province: "广东",
+      // 所在城市
+      city: "深圳",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://s.weibo.com/"
     }
   ],
 
